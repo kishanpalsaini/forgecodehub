94cd1aa92aebf45569852a70bf98cec03786ec62
kishanpalOkoders <kishanpal.saini@okoders.com>
2026-03-28 21:02:39 +0530

    config with calculator

--- next.config.ts
+++ next.config.ts
@@ -14,15 +14,13 @@
 
 // export default nextConfig;
 
-
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: '/calculators/:path*',
-        destination: process.env.NODE_ENV === 'development'
-          ? 'http://localhost:5173/:path*'   // ✅ Vite default port
-          : 'https://finance-calculator-nine-sandy.vercel.app/:path*',
+        // destination: 'http://localhost:5173/:path*',
+        destination: 'https://finance-calculator-nine-sandy.vercel.app/:path*',
       },
     ];
   },
